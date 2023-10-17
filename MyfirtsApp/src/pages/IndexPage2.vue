8507abd459e4d32fa948e7f540fdad221b24c574
greimer29 <greimerabelguzman29@gmail.com>
2023-10-17 12:05:56 -0400

    Objetivo cumplido, insertar un boton en una tabla de quasar con slots, falta funcionalidad del boton

--- MyfirtsApp/src/pages/IndexPage2.vue
+++ MyfirtsApp/src/pages/IndexPage2.vue
@@ -1,19 +1,17 @@
 <template>
-  <div class="q-pa-md" style="max-width: 500px;">
+  <div class="q-pa-md" style="max-width: 700px;">
     <q-table :columns="columnas" :rows="filas" row-key="name">
       <template #body-cell-name="props">
         <q-td :props="props" class="text-italic text-purple">
           {{ props.value }}
         </q-td>
       </template>
-      <template #body-cell="props">
-        <q-td
-          :props="props"
-        >
+      <template #body-cell-opciones="props">
+        <q-td :props="props">
           <q-btn
             flat
             color="primary"
-            :label="props.value"
+            :label="props.row.name"
             @click="copyToClipboard(props.value)"
           />
         </q-td>
@@ -30,21 +28,18 @@ const columnas = ref([
     align: 'center',
     label: 'Nombre',
     field: 'name',
-    sortable: true,
   },
   {
     name: 'apellido',
     align: 'center',
     label: 'Apellido',
     field: 'lastname',
-    sortable: true,
   },
   {
     name: 'edad',
     align: 'center',
     label: 'Edad',
     field: 'age',
-    sortable: true,
   },
   {
     name: 'opciones',
